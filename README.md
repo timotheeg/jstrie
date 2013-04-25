a18db4942817df608300b24cdf79e39f179a485c
Timothee Groleau <tg@timotheegroleau.com>
2013-04-25 21:37:05 +0800

    more tests and CI with travis

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-jstrie
-======
+jstrie [![Build Status](https://travis-ci.org/timotheeg/jstrie.png)](https://travis-ci.org/timotheeg/jstrie)
+=============================================================================================================
 
 Implementing a trie in JavaScript, just for fun
--- test/test_jstrie.js
+++ test/test_jstrie.js
@@ -1,9 +1,9 @@
 var Trie = require('../src/trie.js');
 
-exports.test_building = function(test)
+exports.test_array_of_strings = function(test)
 {
 	var t = new Trie();
-	
+
 	t.build([
 		"tim",
 		"ali",
@@ -32,6 +32,86 @@ exports.test_building = function(test)
 
 	test.equal( t.getWordCount('ali-'), 0 );
 	test.equal( t.getWordCount('foo'), 0 );
+
+	test.done();
+};
+
+exports.test_hash_with_data = function(test)
+{
+	var t = new Trie();
+
+	var inc = 0;
+
+	t.build({
+		"tim": {id: ++inc},
+		"ali": {id: ++inc},
+		"ali-akber": {id: ++inc},
+		"timothee": {id: ++inc},
+		"johan": {id: ++inc},
+		"marc": {id: ++inc}
+	});
+
+	t.addWord("tim", {id: ++inc});
+
+	test.ok( t.hasWord('ali') );
+	test.ok( t.hasWord('ali-akber') );
+	test.ok( t.hasWord('tim') );
+	test.ok( t.hasWord('timothee') );
+	test.ok( t.hasWord('johan') );
+
+	test.ok( !t.hasWord('ali-') );
+	test.ok( !t.hasWord('timo') );
+	test.ok( !t.hasWord('bar') );
+
+	test.equal( t.getWordCount('ali'), 1 );
+	test.equal( t.getWordCount('ali-akber'), 1 );
+	test.equal( t.getWordCount('tim'), 2 );
+	test.equal( t.getWordCount('timothee'), 1 );
+	test.equal( t.getWordCount('johan'), 1 );
+
+	test.equal( t.getWordCount('ali-'), 0 );
+	test.equal( t.getWordCount('foo'), 0 );
+
+	test.done();
+};
+
+exports.test_individual_adds = function(test)
+{
+	var t = new Trie(), inc = 0;
+
+	test.ok( !t.hasWord('ali') );
+	test.equal( t.getWordCount('ali'), 0);
+
+	t.addWord("ali", ++inc);
+
+	test.ok( t.hasWord('ali') );	
+	test.equal( t.getWordCount('ali'), 1);
+
+	t.addWord("ali", ++inc);
+
+	test.ok( t.hasWord('ali') );	
+	test.equal( t.getWordCount('ali'), 2);
+
+	test.deepEqual( t.getWordData('ali'), [1, 2] );
+
+	test.done();
+};
+
+exports.test_data = function(test)
+{
+	var t = new Trie()
+		, inc = 0
+		, data1 = {id: ++inc, dat: 'foo'}
+		, data2 = {id: ++inc, dat: 'bar'}
+		, data3 = {id: ++inc, dat: 'baz'};
+
+	t.addWord("ali", data1);
+	t.addWord("ali", data2);
+	t.addWord("ali-akber", data3);
+
+	test.strictEqual( t.getWordData('ali')[0], data1 );
+	test.strictEqual( t.getWordData('ali')[1], data2 );
+	test.strictEqual( t.getWordData('ali-akber')[0], data3 );
 
 	test.done();
 };
